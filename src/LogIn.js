867c1e6e463bcebd68c23c331b550519d4fe7e0c
dianamariasilva <diana.silva.v94@gmail.com>
2017-10-16 16:13:56 -0500

    resolucion de conflictos

--- src/LogIn.js
+++ src/LogIn.js
@@ -8,3 +8,49 @@ import {
 	Redirect
 } from 'react-router-dom'
 
+class LogIn extends Component {
+    render() {
+      return (
+         <div className="body">
+              <div className="container">
+                  <div className="panel panel-right">
+                      <div className="panel-content">
+                          <div className="form">
+                          <h1>LogIn</h1>
+                          <form>
+                              <div className="group">      
+                                  <input type="email" required className="input" />
+                                  <span className="highlight" />
+                                  <label className="label">
+                                      Email
+                                  </label>
+                              </div>
+                              <div className="group">      
+                                  <input type="password" required className="input" />
+                                  <span className="highlight" />
+                                  <label className="label">
+                                      Password
+                                  </label>
+                              </div>
+                              <button className="log-btn">LOG IN</button>
+                              <a><p>Forgot Password</p></a>
+                              <a><p> Create an account </p></a>
+                          </form>
+                          <div class="row">
+                            <div class="col-md-3 col-sm-3 col-xs-6"> 
+                              <a href="http://www.facebook.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH FACEBOOK</a> 
+                            </div>
+                            <div class="col-md-3 col-sm-3 col-xs-6"> 
+                              <a href="http://www.gmail.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH GOOGLE</a> 
+                            </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+        </div>
+        );
+    }
+}
+  
+export default LogIn;
--- src/SignUp.js
+++ src/SignUp.js
@@ -4,64 +4,6 @@ import './SignUp.css';
 
 
 class SignUp extends Component {
-<<<<<<< HEAD
-  render() {
-    return (
-       <div className="body">
-
-          <div className="container">
-                <div className="panel panel-right">
-                    <div className="panel-content">
-                        <div className="form">
-                        <h1>Create your account</h1>
-                        <form>
-                            <div className="group">      
-                                <input type="text" required className="input" />
-                                <span className="highlight" />
-                                <label className="label">
-                                    First Name
-                                </label>
-                            </div>
-                            <div className="group">      
-                                <input type="TEXT" required className="input" />
-                                <span className="highlight" />
-                                <label className="label">
-                                    Last Name
-                                </label>
-                            </div>
-                            <div className="group">      
-                                <input type="email" required className="input" />
-                                <span className="highlight" />
-                                <label className="label">
-                                    Email
-                                </label>
-                            </div>
-                            <div className="group">      
-                                <input type="password" required className="input" />
-                                <span className="highlight" />
-                                <label className="label">
-                                    Password
-                                </label>
-                            </div>
-                            <div className="group">      
-                                <input type="password" required className="input" />
-                                <span className="highlight" />
-                                <label className="label">
-                                    Confirm Password
-                                </label>
-                            </div>
-                            <button className="log-btn">ADD PROFILE DETAILS</button>
-                            <a><p>Forgot Password</p></a>
-                            <a><p> Create an account </p></a>
-                        </form>
-                        <div class="row">
-                          <div class="col-md-3 col-sm-3 col-xs-6"> 
-                            <a href="http://www.facebook.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH FACEBOOK</a> 
-                          </div>
-                          <div class="col-md-3 col-sm-3 col-xs-6"> 
-                            <a href="http://www.gmail.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH GOOGLE</a> 
-                          </div>
-=======
     render() {
         const BarProgress = () => {
             return (
@@ -155,20 +97,12 @@ class SignUp extends Component {
                                     </div>
                                 </div>
                             </div>
->>>>>>> 1be9ca25e1595512201e4b966ec10717c0eb4716
                         </div>
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
-          </div>
-      </div>
-    );
-  }
-=======
         );
     }
->>>>>>> 1be9ca25e1595512201e4b966ec10717c0eb4716
 }
 
 export default SignUp;
