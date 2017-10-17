38bde441c14762e0325947cac57a30bfa1895ccd
dianamariasilva <diana.silva.v94@gmail.com>
2017-10-17 12:47:31 -0500

    botones facebook funcionamiento y estilo, identando

--- src/LogIn.js
+++ src/LogIn.js
@@ -1,56 +1,81 @@
 import React, { Component } from 'react';
 import './LogIn.css';
 import {
-	BrowserRouter,
-	Route,
-	Switch,
-	NavLink,
-	Redirect
+    BrowserRouter,
+    Route,
+    Switch,
+    NavLink,
+    Redirect
 } from 'react-router-dom'
 
 class LogIn extends Component {
     render() {
-      return (
-         <div className="body">
-            <div className="row col-md-8 col-md-offset-2 registeration pageaccount">
-                <div className="registerInner">
-                    <div className="col-md-6 signUp">
-                          <div className="form">
-                          <h1>LogIn</h1>
-                          <form>
-                              <div className="group">      
-                                  <input type="email" required className="input" />
-                                  <span className="highlight" />
-                                  <label className="label">
-                                      Email
+        return (
+            <div className="body">
+                <div className="row col-md-8 col-md-offset-2 registeration pageaccount">
+                    <div className="registerInner">
+                        <div className="col-md-6 signUp">
+                            <div className="form">
+                                <h1>LogIn</h1>
+                                <form>
+                                    <div className="group">
+                                        <input type="email" required className="input" />
+                                        <span className="highlight" />
+                                        <label className="label">
+                                            Email
                                   </label>
-                              </div>
-                              <div className="group">      
-                                  <input type="password" required className="input" />
-                                  <span className="highlight" />
-                                  <label className="label">
-                                      Password
+                                    </div>
+                                    <div className="group">
+                                        <input type="password" required className="input" />
+                                        <span className="highlight" />
+                                        <label className="label">
+                                            Password
                                   </label>
-                              </div>
-                              <button className="log-btn">LOG IN</button>
-                              <a><p>Forgot Password</p></a>
-                              <a><p> Create an account </p></a>
-                          </form>
-                          <div class="row">
-                            <div class="col-md-3 col-sm-3 col-xs-6"> 
-                              <a href="http://www.facebook.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH FACEBOOK</a> 
+                                    </div>
+                                    <button className="log-btn btn-primary btn">LOG IN</button>
+                                    <div className="text-center">
+                                        <a><p>Forgot Password</p></a>
+                                        <a><p> Create an account </p></a>
+                                    </div>
+                                </form>
+                                <div className="row">
+                                    <div>
+                                        <div className="col-lg-6 col-xs-12">
+                                            <a href="http://www.facebook.com" target="_blank" className="a1">
+                                                <div id="facebook" className="button1">
+                                                    Facebook
+                                    <div className="cover facebook">
+                                                        <li className="fa fa-facebook fa-2x" />
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div className="col-lg-6 col-xs-12">
+                                            <a href="http://www.gmail.com" target="_blank">
+                                                <div id="google" className="button1">
+                                                    Google+
+                                    <div className="cover google">
+                                                        <li className="fa fa-google-plus fa-2x" />
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <link
+                                            href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext"
+                                            rel="stylesheet"
+                                            type="text/css" />
+                                        <link
+                                            href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+                                            rel="stylesheet" />
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-md-3 col-sm-3 col-xs-6"> 
-                              <a href="http://www.gmail.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH GOOGLE</a> 
-                            </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
         );
     }
 }
-  
+
 export default LogIn;
