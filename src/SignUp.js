aff45a62caba155e45d437a3119de76089d064a8
Maria del Rosario Palomino Begazo <100kj.mr@gmail.com>
2017-10-17 01:59:41 -0700

    signup

--- src/SignUp.js
+++ src/SignUp.js
@@ -15,33 +15,33 @@ class SignUp extends Component {
     constructor(props) {
         super(props);
         this.pass = undefined,
-        this.passConfirm = undefined,
-        this.state={
-            password: false
-        }
+            this.passConfirm = undefined,
+            this.state = {
+                password: false
+            }
     }
 
-    inputPass(e){
+    inputPass(e) {
         console.log(e.target.value);
         this.pass = e.target.value;
         this.comparePassword();
     }
 
-    inputPassConfirm(e){
+    inputPassConfirm(e) {
         console.log(e.target.value);
         this.passConfirm = e.target.value;
         this.comparePassword();
     }
 
-    comparePassword(){
-        if(this.pass===this.passConfirm){
+    comparePassword() {
+        if (this.pass === this.passConfirm) {
             this.setState({
-                password:true
+                password: true
             });
         }
-        else{
+        else {
             this.setState({
-                password:false
+                password: false
             });
         }
     }
@@ -81,77 +81,108 @@ class SignUp extends Component {
 
             );
         }
-        return (
-            <div>
-                <Header />
-           
-            <div className="body">
 
+        const FormSignUp = () => {
+            return (
+                <form>
+                    <div className="group">
+                        <input type="text" required className="input" />
+                        <span className="highlight" />
+                        <label className="label">
+                            First Name
+                    </label>
+                    </div>
+                    <div className="group">
+                        <input type="TEXT" required className="input" />
+                        <span className="highlight" />
+                        <label className="label">
+                            Last Name
+            </label>
+                    </div>
+                    <div className="group">
+                        <input type="email" required className="input" />
+                        <span className="highlight" />
+                        <label className="label">
+                            Email
+            </label>
+                    </div>
+                    <div className="group">
+                        <input type="password" required className="input" alue={this.pass}
+                            onChange={(e) => { this.inputPass(e) }} />/>
+                <span className="highlight" />
+                        <label className="label">
+                            Password
+            </label>
+                    </div>
+                    <div className="group">
+                        <input type="password" required className="input" value={this.passConfirm}
+                            onChange={(e) => { this.inputPassConfirm(e) }} />/>
+                <span className="highlight" />
+                        <label className="label">
+                            Confirm Password
+            </label>
+                    </div>
+                    {this.state.password ?
+                        <NavLink to="/datauser">
+                            <button className="findAddress btn btn-primary">ADD PROFILE DETAILS</button>
+                        </NavLink>
+                        :
+                        <button className="findAddress btn btn-primary" disabled>ADD PROFILE DETAILS</button>
+                    }
+                </form>
+            );
+        }
+
+        const ButtonsSocial = () => {
+            return (
+                <div className="row">
+                    <div>
+                        <div className="col-lg-6 col-xs-12">
+                            <a href="http://www.facebook.com" target="_blank" className="a1">
+                                <div id="facebook" className="button1">
+                                    Facebook
+                            <div className="cover facebook">
+                                        <li className="fa fa-facebook fa-2x" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div className="col-lg-6 col-xs-12">
+                            <a href="http://www.gmail.com" target="_blank">
+                                <div id="google" className="button1">
+                                    Google+
+                            <div className="cover google">
+                                        <li className="fa fa-google-plus fa-2x" />
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <link
+                            href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext"
+                            rel="stylesheet"
+                            type="text/css" />
+                        <link
+                            href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+                            rel="stylesheet" />
+                    </div>
+                </div>
+            );
+        }
+
+        return (
+            <div className="body">
                 <div className="row col-md-8 col-md-offset-2 registeration pageaccount">
                     <div className="registerInner">
                         <div className="col-md-6 signUp">
                             <div className="form">
                                 <BarProgress />
-                                <form>
-                                    <div className="group">
-                                        <input type="text" required className="input" />
-                                        <span className="highlight" />
-                                        <label className="label">
-                                            First Name
-                                </label>
-                                    </div>
-                                    <div className="group">
-                                        <input type="TEXT" required className="input" />
-                                        <span className="highlight" />
-                                        <label className="label">
-                                            Last Name
-                                </label>
-                                    </div>
-                                    <div className="group">
-                                        <input type="email" required className="input" />
-                                        <span className="highlight" />
-                                        <label className="label">
-                                            Email
-                                </label>
-                                    </div>
-                                    <div className="group">
-                                        <input type="password" required className="input" value={this.pass}
-                                        onChange={(e) => { this.inputPass(e) }} />
-                                        <span className="highlight" />
-                                        <label className="label">
-                                            Password
-                                </label>
-                                    </div>
-                                    <div className="group">
-                                        <input type="password" required className="input" value={this.passConfirm}
-                                        onChange={(e) => { this.inputPassConfirm(e) }} />
-                                        <span className="highlight" />
-                                        <label className="label">
-                                            Confirm Password
-                                        </label>
-                                    </div>
-                                    {this.state.password ?
-                                        <NavLink to="/datauser">
-                                            <button className="findAddress btn btn-primary">ADD PROFILE DETAILS</button>
-                                        </NavLink>
-                                        :
-                                        <button className="findAddress btn btn-primary" disabled>ADD PROFILE DETAILS</button>
-                                    }
-
-                                </form>
-                                <div class="row">
-                                    <div class="col-md-3 col-sm-3 col-xs-6">
-                                        <a href="http://www.facebook.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH FACEBOOK</a>
-                                    </div>
-                                    <div class="col-md-3 col-sm-3 col-xs-6">
-                                        <a href="http://www.gmail.com" target="_blank" class="btn btn-sm animated-button victoria-four">LOG IN WITH GOOGLE</a>
-                                    </div>
-                                </div>
+                                <FormSignUp />
+                                <ButtonsSocial />
                             </div>
                         </div>
                     </div>
                 </div>
-            </div> </div>
+            </div>
         );
     }
 }
